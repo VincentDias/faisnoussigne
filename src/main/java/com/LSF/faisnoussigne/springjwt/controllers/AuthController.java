1d9ce9618ebaead29be58dc110563dfaf5dad893
VincentDias <vincentdias.pro@gmail.com>
2020-07-23 00:00:14 +0200

    update jwt

--- src/main/java/com/LSF/faisnoussigne/springjwt/controllers/AuthController.java
+++ src/main/java/com/LSF/faisnoussigne/springjwt/controllers/AuthController.java
@@ -34,7 +34,7 @@ import com.LSF.faisnoussigne.springjwt.security.services.UserDetailsImpl;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping()
+@RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
--- src/main/java/com/LSF/faisnoussigne/webservices/article/ArticleController.java
+++ src/main/java/com/LSF/faisnoussigne/webservices/article/ArticleController.java
@@ -43,5 +43,4 @@ public class ArticleController {
         service.deleteArticle(id);
     }
 
-
 }
